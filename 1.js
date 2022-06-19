c1139cbd7cd3a32ef557c979a33f48a82a51582d
alex4doorow <alex4doorow.com>
2022-06-19 14:35:36 +0300

    Started leaning git

---
+++ 1.js
@@ -0,0 +1,5 @@
+<%@ include file="../fragments/header2init.jsp"%>
+<%@ include file="../fragments/header2html2init.jsp"%>
+
+<title><fmt:message key="app.title" /> | <fmt:message
+		key="order.form.update.title" /></title>
