c1eb166ae23b5f3b59b10a115d7b79c43edb779b
alex4doorow <alex4doorow@gmail.com>
2023-01-28 18:57:15 +0300

    refactoring api services

--- sir-richard-boss-lib/src/main/java/ru/sir/richard/boss/api/market/OzonMarketApi.java
+++
@@ -1,4 +0,0 @@
-package ru.sir.richard.boss.api.market;
-
-// https://docs.ozon.ru/api/seller/#tag/Introduction
-public class OzonMarketApi {}
--- sir-richard-boss-lib/src/main/java/ru/sir/richard/boss/api/market/OzonRocketApi.java
+++
@@ -1,9 +0,0 @@
-package ru.sir.richard.boss.api.market;
-
-/**
- * больше не используется, ибо закрыто
- * @author alex4doorow
- *
- */
-public class OzonRocketApi {
-}
